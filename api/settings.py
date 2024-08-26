94017f6f34a01a3cfd93e72c834783ba7d4ae2e4
ChhayaIngle <155736120+ChhayaIngle@users.noreply.github.com>
2024-08-26 16:34:44 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -270,7 +270,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 
 #ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
-ALLOWED_HOSTS = ['dja-5.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['rocket-sales-tracker-4.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
